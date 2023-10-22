539e220eee2b81f76076dad2762aac210e372f71
Oussafadi <fadi.oussa@outlook.fr>
2023-10-22 19:09:42 +0100

    add dependicies

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+
+
+function Navbar() {
+   return ( <>
+    <div>
+        <h1 className="text-3xl text-center font-bold underline">Nav</h1>
+    </div>
+    </>
+   )
+}
+
+
+
+
+export default Navbar
